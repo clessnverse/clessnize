61a5a8d1dc139bf843f7d25e19fd868d9ae663f7
LaurenceOMFoisy <mail@mfoisy.com>
2024-06-18 16:08:20 -0400

    finalement

--- R/ggboule.R
+++ R/ggboule.R
@@ -1,40 +1,57 @@
-#' Add image overlay to bars
+#' Add image overlay to bars or histograms
 #'
 #' @param plot The ggplot object
 #' @param data The data frame containing the counts
 #' @param image_path The path to the image to overlay
+#' @param geom The type of plot: "bar" or "hist"
 #' @return The ggplot object with the image overlay
 #' @noRd
-add_image_bars <- function(plot, data, image_path) {
+add_image_bars <- function(plot, data, image_path, geom) {
   # Load the image
   image <- png::readPNG(image_path)
   
-  for (i in 1:nrow(data)) {
-    plot <- plot + ggplot2::annotation_custom(
-      grid::rasterGrob(image, width = grid::unit(1, "npc"), height = grid::unit(1, "npc"), interpolate = TRUE),
-      xmin = i - 0.5, xmax = i + 0.5, ymin = 0, ymax = data$n[i]
-    )
+  if (geom == "bar") {
+    for (i in 1:nrow(data)) {
+      plot <- plot + ggplot2::annotation_custom(
+        grid::rasterGrob(image, width = grid::unit(1, "npc"), height = grid::unit(1, "npc"), interpolate = TRUE),
+        xmin = i - 0.5, xmax = i + 0.5, ymin = 0, ymax = data$n[i]
+      )
+    }
+  } else if (geom == "hist") {
+    breaks <- hist(data$y, plot = FALSE)$breaks
+    for (i in 1:(length(breaks) - 1)) {
+      plot <- plot + ggplot2::annotation_custom(
+        grid::rasterGrob(image, width = grid::unit(1, "npc"), height = grid::unit(1, "npc"), interpolate = TRUE),
+        xmin = breaks[i], xmax = breaks[i + 1], ymin = 0, ymax = sum(data$y >= breaks[i] & data$y < breaks[i + 1])
+      )
+    }
   }
+  
   return(plot)
 }
 
-#' Create a ggplot with image overlay on bars
+#' Create a ggplot with image overlay on bars or histograms
 #'
 #' @param data The data frame to use for the plot
 #' @param mapping Default list of aesthetic mappings to use for plot
-#' @param image_path The path to the image to overlay on bars
+#' @param image_path The path to the image to overlay on bars or histograms
+#' @param geom The type of plot: "bar" or "hist"
 #' @param ... Other arguments passed to ggplot
-#' @return A ggplot object with the image overlay on bars
+#' @return A ggplot object with the image overlay on bars or histograms
 #' @export
-ggboule <- function(data, mapping = aes(), image_path = system.file("extdata/boule.png", package = "clessnize"), ...) {
+ggboule <- function(data, mapping = aes(), image_path = system.file("extdata/boule.png", package = "clessnize"), geom = "bar", ...) {
   # Create the base ggplot object
   plot <- ggplot2::ggplot(data, mapping, ...)
   
-  # Add geom_bar to the plot
-  plot <- plot + ggplot2::geom_bar(stat = "identity", fill = NA, color = "black")
+  # Add the appropriate geom to the plot
+  if (geom == "bar") {
+    plot <- plot + ggplot2::geom_bar(stat = "identity", fill = NA, color = "black")
+  } else if (geom == "hist") {
+    plot <- plot + ggplot2::geom_histogram(fill = NA, color = "black")
+  }
   
   # Apply the image overlay
-  plot <- add_image_bars(plot, data, image_path)
+  plot <- add_image_bars(plot, data, image_path, geom)
   
   return(plot)
 }
