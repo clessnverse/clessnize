ed93bab03b0e94fba58e9d9917f7bfddbffd0240
hubcad25 <hubertcadieux@gmail.com>
2024-08-19 14:25:10 -0400

    add check_fonts_access

--- R/histoires_datagochi_usa.R
+++ R/histoires_datagochi_usa.R
@@ -1,2 +1,16 @@
+check_fonts_access <- function() {
+  fonts <- c("PixelOperatorSC.ttf", "000webfont.ttf")
+  font_paths <- sapply(fonts, function(font) system.file(paste0("fonts/", font), package = "clessnize"))
+
+  # Vérifier si les chemins ne sont pas vides
+  results <- sapply(font_paths, function(path) if (path == "") "Not found" else "Found")
+  
+  # Créer un message récapitulatif
+  message <- paste(names(results), ":", results, collapse = "\n")
+  
+  return(message)
+}
+
+
 ## put the datagotchi theme here
 
