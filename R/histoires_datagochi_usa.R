53c92d02c9b0fa29a74875397589c43e3d1445d5
hubcad25 <hubertcadieux@gmail.com>
2024-08-19 14:29:12 -0400

    add @export

--- R/histoires_datagochi_usa.R
+++ R/histoires_datagochi_usa.R
@@ -1,3 +1,8 @@
+#' Check if font files are accessible
+#'
+#' This function checks if the specified font files are accessible via system.file().
+#' @return A message indicating whether each font file is found or not.
+#' @export
 check_fonts_access <- function() {
   fonts <- c("PixelOperatorSC.ttf", "000webfont.ttf")
   font_paths <- sapply(fonts, function(font) system.file(paste0("fonts/", font), package = "clessnize"))
