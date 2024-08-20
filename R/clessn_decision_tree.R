a89fd14b5b2479415fce788d541aa5ba064823a6
hubcad25 <hubertcadieux@gmail.com>
2024-08-20 16:02:09 -0400

    tempporary draw_node function

--- R/clessn_decision_tree.R
+++ R/clessn_decision_tree.R
@@ -1,1 +1,12 @@
-### put the code to make decision tree plots here
+#' Draw a Node for Decision Tree Visualization
+#'
+#' This is a temporary function used to plot nodes in a larger decision tree visualization. 
+#' The function currently plots a basic scatter plot as a placeholder.
+#'
+#' @return A plot representing a node in the decision tree.
+#' @export
+draw_node <- function() {
+  plot(mtcars$wt)
+}
+
+
