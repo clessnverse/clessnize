cb97aff6357d7bb0ec2748620a0c813d1cfcdcb9
hubcad25 <hubertcadieux@gmail.com>
2024-08-22 18:04:21 -0400

    change font in draw_datagotchi_node

--- R/clessn_decision_tree.R
+++ R/clessn_decision_tree.R
@@ -80,8 +80,16 @@ draw_datagotchi_node <- function(
       plot.title = element_text(hjust = 0.5),
       legend.position = "none"
     ) +
-    annotate("text", x = 0.5, y = -10, label = left_lab, hjust = 1, size = 5, angle = 90) +
-    annotate("text", x = 0.5, y = 110, label = right_lab, hjust = 1, size = 5, angle = 90) +
+    annotate(
+      "text", x = 0.5, y = -10,
+      label = left_lab, hjust = 1,
+      size = 5, angle = 90, family = "PixelOperatorSC"
+    ) +
+    annotate(
+      "text", x = 0.5, y = 110, 
+      label = right_lab, hjust = 1,
+      size = 5, angle = 90, family = "PixelOperatorSC"
+    ) +
     annotate(
       geom = "rect",
       xmin = -0.122, xmax = -0.053, ymin = 0, ymax = 100,
@@ -93,11 +101,14 @@ draw_datagotchi_node <- function(
     ) +
     annotate(
       "text",
-      x = -0.0875, y = node_proportion * 100, hjust = -0.1, label = paste0(round(node_proportion * 100), "% of sample")
+      x = -0.0875, y = node_proportion * 100, hjust = -0.1,
+      label = paste0(round(node_proportion * 100), "\n% of sample"),
+      family = "PixelOperatorSC"
     ) +
     annotate(
       "text", x = -0.2, y = 50, label = paste(max_group, round(pcts[max_group]), "%"),
-      hjust = 0.5, size = 5, color = max_color, fontface = "bold"
+      hjust = 0.5, size = 5, color = max_color, fontface = "bold",
+      family = "PixelOperatorSC"
     )  
   return(plot)
 }
