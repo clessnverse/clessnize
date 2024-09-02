a31d650d7b30fe4bbdfbfc504b527d3dfebd9ad1
hubcad25 <hubertcadieux@gmail.com>
2024-09-02 16:05:52 -0400

    add state_coords for a cartogram

---
+++ R/cartogram.R
@@ -0,0 +1,59 @@
+#' State Coordinates Cartogram
+#'
+#' A list containing the x and y coordinates for each U.S. state to create a cartogram.
+#'
+#' @format A list of length 50 where each element is a named vector with two numeric values: `x` and `y`.
+#' @export
+state_coordinates <- list(
+  AL = c(x = 0.9, y = 0.3),   # Alabama
+  AK = c(x = 0, y = 0.9),    # Alaska
+  AZ = c(x = 0.15, y = 0.4),   # Arizona
+  AR = c(x = 0.6, y = 0.4),   # Arkansas
+  CA = c(x = 0, y = 0.4),   # California
+  CO = c(x = 0.3, y = 0.5),   # Colorado
+  CT = c(x = 1.35, y = 0.7),    # Connecticut
+  DE = c(x = 1.35, y = 0.5),  # Delaware
+  DC = c(x = 1.2, y = 0.5),  # District of Columbia
+  FL = c(x = 1.05, y = 0.2),   # Florida
+  GA = c(x = 1.05, y = 0.3),   # Georgia
+  HI = c(x = 0, y = 0.2),   # Hawaii
+  ID = c(x = 0.15, y = 0.6),   # Idaho
+  IL = c(x = 0.75, y = 0.5),    # Illinois
+  IN = c(x = 0.9, y = 0.5),  # Indiana
+  IA = c(x = 0.6, y = 0.6),  # Iowa
+  KS = c(x = 0.45, y = 0.5),    # Kansas
+  KY = c(x = 0.9, y = 0.4),  # Kentucky
+  LA = c(x = 0.6, y = 0.3),   # Louisiana
+  ME = c(x = 1.5, y = 0.8),   # Maine
+  MD = c(x = 1.2, y = 0.6),  # Maryland
+  MA = c(x = 1.5, y = 0.6),   # Massachusetts
+  MI = c(x = 0.9, y = 0.6),    # Michigan
+  MN = c(x = 0.6, y = 0.7),    # Minnesota
+  MS = c(x = 0.75, y = 0.3),    # Mississippi
+  MO = c(x = 0.6, y = 0.5),  # Missouri
+  MT = c(x = 0.15, y = 0.7),    # Montana
+  NE = c(x = 0.45, y = 0.6),   # Nebraska
+  NV = c(x = 0, y = 0.5),    # Nevada
+  NH = c(x = 1.5, y = 0.7),  # New Hampshire
+  NJ = c(x = 1.35, y = 0.6),   # New Jersey
+  NM = c(x = 0.3, y = 0.4),    # New Mexico
+  NY = c(x = 1.2, y = 0.8),   # New York
+  NC = c(x = 1.2, y = 0.3),    # North Carolina
+  ND = c(x = 0.45, y = 0.8),  # North Dakota
+  OH = c(x = 1.05, y = 0.5),   # Ohio
+  OK = c(x = 0.45, y = 0.4),   # Oklahoma
+  OR = c(x = 0, y = 0.6),   # Oregon
+  PA = c(x = 1.2, y = 0.7),   # Pennsylvania
+  RI = c(x = 1.5, y = 0.5),   # Rhode Island
+  SC = c(x = 1.2, y = 0.2),   # South Carolina
+  SD = c(x = 0.45, y = 0.7),   # South Dakota
+  TN = c(x = 0.75, y = 0.4),   # Tennessee
+  TX = c(x = 0.45, y = 0.3),  # Texas
+  UT = c(x = 0.15, y = 0.5),  # Utah
+  VT = c(x = 1.35, y = 0.8),  # Vermont
+  VA = c(x = 1.2, y = 0.4),  # Virginia
+  WA = c(x = 0, y = 0.7),   # Washington
+  WV = c(x = 1.05, y = 0.4),   # West Virginia
+  WI = c(x = 0.75, y = 0.6),    # Wisconsin
+  WY = c(x = 0.3, y = 0.6)    # Wyoming
+)
